171d9156edcc07752dedd993929ecf71ffcf1331
OneTooMany <santosismael.115@gmail.com>
2023-07-24 15:43:15 -0700

    Config jest for webpack

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
+  "\\.[jt]sx?$": "babel-jest",
   transform: {},
+  modulePaths: ["/shared/vendor/modules"],
+  moduleFileExtensions: ["js", "jsx"],
+  moduleDirectories: ["node_modules", "bower_components", "shared"],
+
+  moduleNameMapper: {
+    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
+    "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js",
+    "^react(.*)$": "<rootDir>/vendor/react-master$1",
+    "^config$": "<rootDir>/configs/app-config.js",
+  },
 };
