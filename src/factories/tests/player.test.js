0ce99bcee17db1ff5c4d535af1b2c979fcbf4806
OneTooMany <santosismael.115@gmail.com>
2023-07-30 13:17:39 -0700

    Create test for proper attack of enemy board

--- src/factories/tests/player.test.js
+++ src/factories/tests/player.test.js
@@ -13,4 +13,100 @@ describe("Player", () => {
   test("creates new player", () => {
     expect(player).toEqual({ name: "Player One" });
   });
+
+  test("attacks enemy", () => {
+    enemyGameBoard.placeShip(2, 2, 5, "vertical");
+    expect(player.attackShip(2, 2, enemyGameBoard)).toEqual([
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [
+        0,
+        0,
+        {
+          hits: 1,
+          id: "ship_2_2",
+          length: 5,
+          status: false,
+        },
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+      ],
+      [
+        0,
+        0,
+        {
+          hits: 1,
+          id: "ship_2_2",
+          length: 5,
+          status: false,
+        },
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+      ],
+      [
+        0,
+        0,
+        {
+          hits: 1,
+          id: "ship_2_2",
+          length: 5,
+          status: false,
+        },
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+      ],
+      [
+        0,
+        0,
+        {
+          hits: 1,
+          id: "ship_2_2",
+          length: 5,
+          status: false,
+        },
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+      ],
+      [
+        0,
+        0,
+        {
+          hits: 1,
+          id: "ship_2_2",
+          length: 5,
+          status: false,
+        },
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+      ],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
+  });
 });
