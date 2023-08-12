ee7ceb0ae3d861294b4cf3620662b09d993ebf1a
OneTooMany <santosismael.115@gmail.com>
2023-08-11 17:01:08 -0700

    Enhance player interaction and turn based logic
    Add variables to track current player & opposing board
    
    Create function to handle cell click & attack based on current player

--- src/game.js
+++ src/game.js
@@ -14,6 +14,9 @@ const game = () => {
 
   createGameBoardGrid(playerOneBoard.board, playerTwoBoard.board);
 
+  let currentPlayer = playerOne;
+  let currentGameBoard = playerTwoBoard;
+
   // listen for cell click
   document.addEventListener("click", (e) => {
     if (e.target.closest(".grid")) {
@@ -23,17 +26,26 @@ const game = () => {
 
   const getCoordinates = (coordinates) => {
     let iter = 0;
+    let xCoord;
+    let yCoord;
 
     for (const attr in coordinates) {
       if (iter === 0) {
-        const yCoord = coordinates[attr];
+        yCoord = coordinates[attr];
         iter++;
         console.log(coordinates[attr[0]]);
       } else if (iter === 1) {
-        const xCoord = coordinates[attr];
+        xCoord = coordinates[attr];
         iter++;
       }
     }
+    handleCellClick(xCoord, yCoord);
+  };
+
+  const handleCellClick = (x, y) => {
+    console.log(x, y);
+    currentPlayer.attackShip(x, y, currentGameBoard);
+    createGameBoardGrid(playerOneBoard.board, playerTwoBoard.board);
   };
 };
 
