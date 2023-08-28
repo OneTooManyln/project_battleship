66e1a4caa0e264f16325df5ee778255ed8878330
OneTooMany <santosismael.115@gmail.com>
2023-08-28 16:47:16 -0700

    Add attack validity check
    Enhance player class to include check for a legal attack

--- src/factories/player.js
+++ src/factories/player.js
@@ -3,7 +3,20 @@ export default class player {
     this.name = name;
   }
 
+  isAttackValid(x, y, gameBoard) {
+    const cell = gameBoard.board[x][y];
+    if (cell === 1 || (cell[1] && cell[1].hitMarker === "x")) {
+      return false;
+    }
+
+    return true;
+  }
+
   attackShip(x, y, gameBoard) {
+    if (!this.isAttackValid(x, y, gameBoard)) {
+      return false;
+    }
+
     gameBoard.recieveAttack(x, y);
     return gameBoard.board;
   }
